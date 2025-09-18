771c35e43b5c0b7101e5ba4572352e1d9d3f4794
Stefan Tananov <s.tananov@yahoo.com>
2025-09-18 13:04:47 +0300

    Update api.js

--- api.js
+++ api.js
@@ -2,8 +2,8 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 let supabase;
 export function initClient(){
-  const SUPABASE_URL = '<YOUR_SUPABASE_URL>';
-  const SUPABASE_ANON_KEY = '<YOUR_SUPABASE_ANON_KEY>';
+  const SUPABASE_URL = 'https://wwdshtnrduxauwxtgadl.supabase.co';
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3ZHNodG5yZHV4YXV3eHRnYWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxODQ5NDUsImV4cCI6MjA3Mzc2MDk0NX0.R_ITamQoP4G6webhXg-q81iywl3nzmysRBDx6YwJKNw';
   supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 }
 export function client(){ return supabase; }
